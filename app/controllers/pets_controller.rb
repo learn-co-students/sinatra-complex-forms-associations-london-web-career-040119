e64ce1c4f07198e24ac1f63ca894928929c46552
Ivan Szebenszki <szib77@gmail.com>
2019-04-25 13:36:08 +0100

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,25 +1,49 @@
 class PetsController < ApplicationController
-
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    owner = if params['owner_name'].empty?
+              Owner.find(params['owner_id'])
+            else
+              Owner.create(name: params['owner_name'])
+            end
+
+    @pet = Pet.create(name: params['pet_name'], owner: owner)
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do
+    # puts params
+    # binding.pry
+    @pet = Pet.find(params[:id])
+
+    owner = if params[:owner][:name].empty?
+              Owner.find(params[:owner_id])
+            else
+              Owner.create(params[:owner])
+            end
+
+    @pet.update(name: params[:pet_name], owner: owner)
 
     redirect to "pets/#{@pet.id}"
   end
